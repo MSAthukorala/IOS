8dd3f312eaaa325c2195f9a602516ed75fb90f1b
MSAthukorala <39034270+MSAthukorala@users.noreply.github.com>
2021-02-07 15:11:55 +0530

    Add files via upload

---
+++ iOS_Movindya Athukorala-COBSCComp192P-050/Movindya Athukorala-COBSCComp192P-050/Movindya Athukorala-COBSCComp192P-050/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImashaHansini-COBSCComp192P-018
+//
+//  Created by Imasha on 2/5/21.
+//  Copyright © 2021 NIBM 19.2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
